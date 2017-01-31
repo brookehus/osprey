9a69c17efeed6f225947523bb59d67b25af3dc2a
Brooke Husic <bhusic@stanford.edu>
2017-01-31 10:08:56 -0800

    best model by median

--- osprey/execute_currentbest.py
+++ osprey/execute_currentbest.py
@@ -12,22 +12,48 @@ def execute(args, parser):
     session = config.trials()
 
     items = [curr.to_dict() for curr in session.query(Trial).all()]
+
+    #print(items)
+
     if not items:
         print('No Models Found')
     else:
-        mean_test_scores = np.nan_to_num(np.array([i["mean_test_score"]
+        # mean_test_scores = np.nan_to_num(np.array([i["mean_test_score"]
+        #                                           for i in items],
+        #                                           dtype=float))
+        # c_b_m = items[mean_test_scores.argmax()]
+        # parameter_dict = c_b_m["parameters"]
+        # weighted_mean = c_b_m["mean_test_score"]
+        # raw_test_scores = np.array(c_b_m["test_scores"])
+        # raw_test_weights = np.array(c_b_m["n_test_samples"])
+        # weighted_var = np.average((raw_test_scores - weighted_mean)**2, weights=raw_test_weights)
+        # weighted_std = np.sqrt(weighted_var)
+        # print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        # print('Best Current Model = %f +- %f' % (weighted_mean,
+        #                                          weighted_std))
+
+        all_test_scores = np.nan_to_num(np.array([i["test_scores"]
                                                   for i in items],
                                                   dtype=float))
-        c_b_m = items[mean_test_scores.argmax()]
-        parameter_dict = c_b_m["parameters"]
-        weighted_mean = c_b_m["mean_test_score"]
-        raw_test_scores = np.array(c_b_m["test_scores"])
-        raw_test_weights = np.array(c_b_m["n_test_samples"])
-        weighted_var = np.average((raw_test_scores - weighted_mean)**2, weights=raw_test_weights)
-        weighted_std = np.sqrt(weighted_var)
+        all_medians = np.median(all_test_scores, axis=1)
+        zipped_list = zip(all_medians, all_test_scores)
+        maxmed = all_test_scores[all_medians.argmax()]
+        cbm_index = [index for index, i in enumerate(items) if np.all(i["test_scores"] == maxmed)]
+
+        cbm = items[cbm_index[0]]
+        parameter_dict = cbm["parameters"]
+        median = np.median(cbm["test_scores"])
+        q75, q25 = np.percentile(cbm["test_scores"], [75 ,25])
+        iqrange = q75 - q25
+
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-        print('Best Current Model = %f +- %f' % (weighted_mean,
-                                                 weighted_std))
+        print('Best Current Model: median = %f' % median)
+        print('                    inter-quartile range = %f' % iqrange)
+        print('                    scores = ')
+        for score in cbm["test_scores"]:
+            print('                             ',score)
+
+
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         if isinstance(config.estimator(), Pipeline):
             print('PipelineStep\tParameter \t Value')
