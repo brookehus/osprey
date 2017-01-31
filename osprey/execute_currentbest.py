4170c6e0b53da3064f9bfae47078bba3c321ed20
Brooke Husic <bhusic@stanford.edu>
2017-01-31 12:55:53 -0800

    accounts for test_scores is None

--- osprey/execute_currentbest.py
+++ osprey/execute_currentbest.py
@@ -32,13 +32,25 @@ def execute(args, parser):
         # print('Best Current Model = %f +- %f' % (weighted_mean,
         #                                          weighted_std))
 
+        # all_test_scores = np.nan_to_num(np.array([i["test_scores"]
+        #                                           for i in items],
+        #                                           dtype=float))
+
+        # for i in items:
+        #     print(i["test_scores"])
+        #     print(i["test_scores"] == None)
+
         all_test_scores = np.nan_to_num(np.array([i["test_scores"]
-                                                  for i in items],
+                                                  for i in items
+                                                  if i["test_scores"] is not None],
                                                   dtype=float))
+
         all_medians = np.median(all_test_scores, axis=1)
         zipped_list = zip(all_medians, all_test_scores)
         maxmed = all_test_scores[all_medians.argmax()]
-        cbm_index = [index for index, i in enumerate(items) if np.all(i["test_scores"] == maxmed)]
+        cbm_index = [index for index, i in enumerate(items)
+                     if i["test_scores"] is not None
+                     and np.all(i["test_scores"] == maxmed)]
 
         cbm = items[cbm_index[0]]
         parameter_dict = cbm["parameters"]
